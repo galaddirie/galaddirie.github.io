72b7a6e4068546667b7697d4ed59d2f150eab485
galaddirie <70884733+galaddirie@users.noreply.github.com>
2023-03-17 02:27:02 -0400

    Create Projects.tsx

---
+++ src/pages/Projects/Projects.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card() {
+
+}
+
+export function Projects() {
+    return (
+        <section id="projects-container" className="projects text-light" style={{ paddingTop: "150px", paddingBottom: "150px" }}>
+            <div id="projects" className="container">
+                <h2>PROJECTS</h2>
+            </div>
+        </section>
+
+    );
+};
