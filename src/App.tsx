45d4ab1c25901290de99f37a77760be38eececd9
Galad Dirie <galad.work@gmail.com>
2023-08-20 19:21:14 -0400

    Update: Refactor scrollspy to navigation context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 import { useState } from 'react'
 import reactLogo from '@assets/test.png'
 import { Navbar } from '@components/Navbar/Navbar'
-import { ScrollSpy } from '@components/ScrollSpy/ScrollSpy'
 
 import { Home } from '@pages/Home/Home'
 import { Projects } from '@pages/Projects/Projects'
 import { Element as Section } from 'react-scroll'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { NavigationProvider } from '@src/contexts/NavigationContext';
+
 //import '@assets/css/style.css'
 import './App.css'
 import '@assets/css/card.css'
 import "@src/index.scss";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 interface SectionProps {
   name: string
 }
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Router>
-        <ScrollSpy>
+      <NavigationProvider>
           <Navbar />
           <main id="main">
             <div className="main-container">
@@ -41,8 +42,7 @@ function App() {
             </div>
           </main>
 
-        </ScrollSpy>
-
+      </NavigationProvider>
       </Router>
     </>
   )
---
+++ src/contexts/NavigationContext.tsx
@@ -0,0 +1,71 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { Events, scroller, scrollSpy } from 'react-scroll';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+interface NavigationContextProps {
+    preventNavUpdate: boolean;
+    setPreventNavUpdate: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const NavigationContext = createContext<NavigationContextProps | undefined>(undefined);
+
+export const useNavigation = () => {
+    const context = useContext(NavigationContext);
+    if (!context) {
+        throw new Error("useNavigation must be used within a NavigationProvider");
+    }
+    return context;
+};
+
+interface NavigationProviderProps {
+    children: React.ReactNode;
+}
+
+export const NavigationProvider: React.FC<NavigationProviderProps> = ({ children }) => {
+    const [preventNavUpdate, setPreventNavUpdate] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        Events.scrollEvent.register('begin', (to: string) => {
+            setPreventNavUpdate(true);
+        });
+
+        Events.scrollEvent.register('end', (to: string) => {
+            setPreventNavUpdate(false);
+            if (!preventNavUpdate && to !== location.pathname.replace(/^\/+/, '')) {
+                navigate(`/${to}`);
+                
+            }
+        });
+        
+        scrollSpy.update();
+
+        return () => {
+            Events.scrollEvent.remove('begin');
+            Events.scrollEvent.remove('end');
+        }
+    }, [preventNavUpdate]);
+
+    useEffect(() => {
+        const hash = location.pathname.replace(/^\/+/, '');
+        if (hash) {
+            scroller.scrollTo(hash, {
+                duration: 100,
+                delay: 0,
+                smooth: 'linear',
+                offset: 20
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log(`preventNavUpdate: ${preventNavUpdate}`);
+    }, [preventNavUpdate]);
+
+    return (
+        <NavigationContext.Provider value={{ preventNavUpdate, setPreventNavUpdate }}>
+            {children}
+        </NavigationContext.Provider>
+    );
+};
