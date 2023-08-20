5ace1302f922075a0e342a6f237d588241b70bc5
Galad Dirie <galad.work@gmail.com>
2023-08-20 19:23:26 -0400

    Update: Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -21,52 +21,52 @@ export function Home() {
 
     return (
         <section id="hero">
-            <div className="hero-container" style={{ height: '100%' }}>
+            <div className="hero-container" style={{ height: '100vh' }}>
                 <div className="video-container">
-                    <img src="assets/img/hero.jpg" alt="hero" className="heroVideo video-thumbnail" style={setThumbnailStye()} />
-                    {/* <LazyLoadComponent> */}
+                    <img src="src/assets/img/hero.jpg" alt="hero" className="heroVideo video-thumbnail" style={setThumbnailStye()} />
+                    <LazyLoadComponent>
                     <video autoPlay muted loop className="heroVideo" onLoadedData={onLoadedData}>
-                        <source src="assets/video/video_compressed.webm" type="video/webm" />
+                        <source src="src/assets/video/video_compressed.webm" type="video/webm" />
                     </video>
-                    {/* </LazyLoadComponent> */}
+                    </LazyLoadComponent>
 
 
                 </div>
 
-                <div className="hero-intro container d-flex align-items-center" style={{ height: '100%' }}>
-                    <div className="row">
-                        <div className="mx-lg-auto text-lg-start text-center col-12 d-flex align-items-center">
-                            <div className="d-flex flex-column text-lg-start text-center ">
-                                <h1 style={{ textShadow: "0px 5px 5px #000000" }}>Galad Dirie<span>.</span></h1>
-                                <h2 style={{ fontStyle: "italic" }}>Full Stack Developer<span>.</span>
-                                </h2>
-                                <div className="social-links d-flex text-lg-start text-center mx-auto mx-lg-0 ">
 
-                                    <a className="pe-4 text-lg" href="https://www.linkedin.com/in/galad-dirie/" style={{ fontSize: "2rem", fontStyle: "italic", color: "crimsion" }}>
-                                        <i className="bi bi-linkedin"></i>
-                                    </a>
-                                    <a className="text-lg" href="https://github.com/galaddirie" style={{ fontSize: "2rem", fontStyle: "italic", color: "crimsion" }}>
-                                        <i className="bi bi-github"></i>
-                                    </a>
+            </div>
+            <div className="hero-intro container d-flex align-items-center" style={{ height: '100%' }}>
+                <div className="row">
+                    <div className="mx-lg-auto text-lg-start text-center col-12 d-flex align-items-center">
+                        <div className="d-flex flex-column text-lg-start text-center ">
+                            <h1 style={{ textShadow: "0px 5px 5px #000000" }}>Galad Dirie<span>.</span></h1>
+                            <h2 style={{ fontStyle: "" }}>Full Stack Developer<span>.</span>
+                            </h2>
+                            <div className="social-links d-flex text-lg-start text-center mx-auto mx-lg-0 ">
 
-                                </div>
-                                <p style={{ fontStyle: "italic", color: "rgb(163, 163, 163)" }}>
-                                    galad.work@gmail.com</p>
-                                <div style={{ width: "100%" }}>
-                                    <Link to="projects" spy={true} smooth={true} offset={-50} duration={500}>
-                                        <button className="btn btn-outline-danger text-light">
-                                            See Projects <i className="bi bi-arrow-down-circle-fill"></i>
-                                        </button>
-                                    </Link>
-
-                                </div>
+                                <a className="pe-4 text-lg" href="https://www.linkedin.com/in/galad-dirie/" style={{ fontSize: "2rem", fontStyle: "italic", color: "crimsion" }}>
+                                    <i className="bi bi-linkedin"></i>
+                                </a>
+                                <a className="text-lg" href="https://github.com/galaddirie" style={{ fontSize: "2rem", fontStyle: "italic", color: "crimsion" }}>
+                                    <i className="bi bi-github"></i>
+                                </a>
 
                             </div>
+                            <p style={{ fontStyle: "italic", color: "rgb(163, 163, 163)" }}>
+                                galad.work@gmail.com</p>
+                            <div style={{ width: "100%" }}>
+                                <Link to="projects" spy={true} smooth={true} offset={20} duration={500}>
+                                    <button className="btn btn-outline-danger text-light">
+                                        See Projects <i className="bi bi-arrow-down-circle-fill"></i>
+                                    </button>
+                                </Link>
+
+                            </div>
+
                         </div>
                     </div>
                 </div>
             </div>
-
         </section >
     );
 }
